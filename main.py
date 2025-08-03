1e227128eca3387c0c4c7cdaa2508161f73b2f89
HalleyWorkspace <linkhalley@comcast.net>
2025-08-03 13:04:32 -0400

    created char_counts function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_words
+from stats import num_words,char_counts
 
 def get_book_text(file):
     with open(file) as f:
@@ -9,5 +9,6 @@ def main():
     book_text=get_book_text(file_path)
     word_count=num_words(book_text)
     print(f"{word_count} words found in the document")
+    print(char_counts(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def num_words(book_text):
     text_list=book_text.split()
     return len(text_list)
+
+def char_counts(book_text):
+    count_dict = {}
+    for char in book_text.lower():
+        if char not in count_dict.keys():
+            count_dict[char]=1
+        else:
+            count_dict[char]+=1
+    return count_dict
