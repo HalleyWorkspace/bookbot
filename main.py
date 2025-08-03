05951ff73701a1ed6f9d49c71b742426d5b5b74b
HalleyWorkspace <linkhalley@comcast.net>
2025-08-03 12:41:39 -0400

    moved num_words to stats.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import num_words
+
 def get_book_text(file):
     with open(file) as f:
         return f.read()
-
-def num_words(book_text):
-    text_list=book_text.split()
-    return len(text_list)
 
 def main():
     file_path="books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,3 @@
+def num_words(book_text):
+    text_list=book_text.split()
+    return len(text_list)
