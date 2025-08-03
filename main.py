108fe40b22c3ba6f12b6641c81eefc02c9f47e1b
HalleyWorkspace <linkhalley@comcast.net>
2025-08-03 12:33:51 -0400

    create num_words function

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(file):
     with open(file) as f:
         return f.read()
 
+def num_words(book_text):
+    text_list=book_text.split(" ")
+    return len(text_list)
+
 def main():
     file_path="books/frankenstein.txt"
-    print(get_book_text(file_path))
+    book_text=get_book_text(file_path)
+    word_count=num_words(book_text)
+    print(f"{word_count} words found in the document")
 
 main()
